86eb2e13a132a5eff64c525c5be67370e15b3bde
Gabrielė Barsukova <g.barsukova@adform.com>
2024-01-05 19:45:36 +0200

    changes doc

--- src/WebAPI/Controllers/AuthenticateController.cs
+++ src/WebAPI/Controllers/AuthenticateController.cs
@@ -7,17 +7,29 @@ using WebAPI.Services;
 
 namespace WebAPI.Controllers;
 
+/// <summary>
+/// Controller for handling authentication-related operations.
+/// </summary>
 [ApiController]
 [Route("v1/[controller]")]
 public class AuthenticateController : ControllerBase
 {
     private readonly AuthenticateService _authenticateService;
 
+    /// <summary>
+    /// Initializes a new instance of the AuthenticateController class.
+    /// </summary>
+    /// <param name="authenticateService">The service handling authentication.</param>
     public AuthenticateController(AuthenticateService authenticateService)
     {
         _authenticateService = authenticateService;
     }
 
+    /// <summary>
+    /// Authenticates a user based on the provided login information.
+    /// </summary>
+    /// <param name="user">The user's login credentials.</param>
+    /// <returns>A response indicating the authentication status.</returns>
     [AllowAnonymous]
     [HttpPost("login")]
     public async Task<IActionResult> Authenticate(UserLogin user)
